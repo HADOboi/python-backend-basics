5961389bbb89d25e091fcddcc32fc97ac110485f
Abdul Haadi <haadikkd24@gmail.com>
2026-07-02 19:43:09 +0530

    inheritance basics

--- student_class.py
+++ student_class.py
@@ -5,14 +5,25 @@ class Student:
         self.course = course
 
     def display_details(self):
-        print("\n\n")
+        print("\n\nSTUDENT DETAILS")
+        print("---------------")
         print("Name : ", self.name)
         print("Age : ", self.age)
         print("Course : ", self.course)
+
+class GraduateStudent(Student):
+    def __init__(self, name, age, course, specialization):
+        super().__init__(name, age,course)
+        self.specialization = specialization
+
+    def display_details(self):
+        super().display_details()
+        print("Specialization : ", self.specialization)
     
 name = input("enter student name: ")
 age = int(input("enter sutdent age: "))
 course = input("enter course: ")
+specialization = input("enter specialization: ")
 
-student1 = Student(name, age, course)
+student1 = GraduateStudent(name, age, course, specialization)
 student1.display_details()
