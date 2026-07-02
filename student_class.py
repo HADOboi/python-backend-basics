1c08b82704d6c38c0f91f141a2e9bece7b356819
Abdul Haadi <haadikkd24@gmail.com>
2026-07-02 19:56:44 +0530

    Encapsulation basics

--- student_class.py
+++ student_class.py
@@ -1,8 +1,23 @@
 class Student:
     def __init__(self, name, age, course):
-        self.name = name
-        self.age = age
-        self.course = course
+        self.__name = name
+        self.__age = age
+        self.__course = course
+
+        def get_name(self):
+            return self.__name
+
+        def get_age(self):
+            return self.__age
+
+        def get_course(self):
+            return self.__course
+
+        def set_age(self, age):
+            if age > 0:
+                self.__age = age
+            else:
+                print("Invalid age")
 
     def display_details(self):
         print("\n\nSTUDENT DETAILS")
@@ -14,11 +29,11 @@ class Student:
 class GraduateStudent(Student):
     def __init__(self, name, age, course, specialization):
         super().__init__(name, age,course)
-        self.specialization = specialization
+        self.__specialization = specialization
 
     def display_details(self):
         super().display_details()
-        print("Specialization : ", self.specialization)
+        print("Specialization : ", self.__specialization)
     
 name = input("enter student name: ")
 age = int(input("enter sutdent age: "))
